bcbc33dde980554c1ef9a37aeb81c2fc33109381
Tolga Boztuna <tboztuna@yahoo.com>
2019-01-13 13:59:59 +0300

    "--force" option is added to build_html bash script. If you pass "--force" option to build html files, it will be regenerated.

--- build_html.sh
+++ build_html.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -d "./BookHTML" ] && [ "$1" == "--force" ]
+then
+	echo "Removing the old files."
+	rm -rf ./BookHTML/
+fi
+
 sphinx-build -b html -d BookHTML/doctrees Book BookHTML/html
